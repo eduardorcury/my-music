dff780d101fdbaf90bffcb0de8230118e3f62e37
eduardorcury <eduardoribeirocury@gmail.com>
2022-05-29 17:51:25 -0400

    adicionando barra de pesquisa

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime, map, Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -9,12 +10,21 @@ import { FormControl } from '@angular/forms';
 export class MainComponent implements OnInit {
 
   searchControl: FormControl;
+  subject: Subject<String>;
+  formValue: String = '';
 
   constructor() { 
     this.searchControl = new FormControl('');
+    this.subject = new Subject();
+    // this.formValue = new Observable();
+    this.searchControl.valueChanges.subscribe(value => this.subject.next(value));
   }
 
   ngOnInit(): void {
+    this.subject.pipe(
+      debounceTime(1000), 
+      map(text => this.formValue = text)
+    );
   }
 
 }

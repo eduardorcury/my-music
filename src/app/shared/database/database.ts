ef4b33bcdd23e76f2f56f79cdc0c4016d75372cd
eduardorcury <eduardoribeirocury@gmail.com>
2022-06-19 12:14:03 -0400

    adicionando banco em memória

---
+++ src/app/shared/database/database.ts
@@ -0,0 +1,23 @@
+import Dexie, { Table } from "dexie";
+
+export class Database extends Dexie {
+
+    albuns!: Table<DatabaseAlbum, number>;
+
+    constructor() {
+        super('ngdexieliveQuery');
+        this.version(1).stores({
+            albuns: '++id'
+        });
+    }
+    
+}
+
+export interface DatabaseAlbum {
+    nome: string,
+    uriSpotify: string,
+    urlImagem: string,
+    artistas: string[]
+}
+
+export const database = new Database();

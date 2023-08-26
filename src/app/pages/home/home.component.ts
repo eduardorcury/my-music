6ebe9495cb7bd58ad81aae0b1762d4c88d8bccbe
eduardorcury <eduardoribeirocury@gmail.com>
2023-08-26 14:10:37 -0400

    fluxo login spotify

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { SpotifyService } from 'src/app/shared/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,15 @@ import { FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
+  constructor(private spotifyService: SpotifyService) { 
   }
 
   ngOnInit(): void {
+  }
+
+  autorizar() {
+    console.log("teste");
+    this.spotifyService.login().subscribe();
   }
 
 }
--- src/app/shared/services/spotify.service.ts
+++ src/app/shared/services/spotify.service.ts
@@ -9,6 +9,11 @@ export class SpotifyService {
 
   constructor(private http: HttpClient) { }
 
+  login() {
+    console.log("teste2");
+    return this.http.get("http://ec2-15-229-69-255.sa-east-1.compute.amazonaws.com:8080/login");
+  }
+
   search(query: string) {
     return this.http.get<AlbumDTO[]>("http://ec2-15-229-69-255.sa-east-1.compute.amazonaws.com:8080/pesquisa", { 
       params: { 
